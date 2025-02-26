1faedeb1b112e4fbf776e2ad393d8e60a15e2d16
lucasphill <lucaspscp@outlook.com>
2025-02-26 16:38:03 -0300

    Init: full login screen with http request

---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  imports: [],
+  templateUrl: './default-login-layout.component.html',
+  styleUrl: './default-login-layout.component.scss'
+})
+export class DefaultLoginLayoutComponent {
+  @Input() title: string = "";
+  @Input() primaryButtonText: string = "";
+  @Input() secondaryButtonText: string = "";
+  @Output("submit") onSubmit = new EventEmitter();
+  @Output("navigate") onNavigate = new EventEmitter();
+
+  submit() {
+    this.onSubmit.emit();
+  }
+
+  navigate() {
+    this.onNavigate.emit();
+  }
+}
